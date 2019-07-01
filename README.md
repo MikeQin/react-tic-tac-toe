f15e788e868b100720798230963b6660c6cd81b0
MikeQin <mike.qin@gmail.com>
2019-07-01 02:17:54 -0400

    init

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# React Tic-Tac-Toe Game
+
+## Tutorial [Link](https://reactjs.org/tutorial/tutorial.html)
+
+----
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ src/components/Game.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import Board from "./Board";
+import calculateWinner from "../functions/calculateWinner";
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{ squares: Array(9).fill(null) }],
+      xIsNext: true,
+      stepNumber: 0
+    };
+  }
+
+  handleClick = i => {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState(prevState => ({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      xIsNext: !prevState.xIsNext,
+      stepNumber: history.length
+    }));
+  };
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
+    });
+  }
+
+  reset = () => {
+    this.setState({
+      history: [{ squares: Array(9).fill(null) }],
+      xIsNext: true,
+      stepNumber: 0
+    });
+    console.log("State is initialized ...");
+  };
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, i) => {
+      const desc = i ? "Go to move #" + i : "Go to game start";
+      return (
+        <li key={i}>
+          <button onClick={() => this.jumpTo(i)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
+        </div>
+        <div className="game-info">
+          <div>
+            <button onClick={() => this.reset()}>Reset the game</button>
+          </div>
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
